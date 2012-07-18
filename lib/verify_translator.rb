040791cb72fc8f20c24a7f8445f7d75138d7cfea
Josh Carter <public@joshcarter.com>
2012-07-18 15:40:17 -0600

    Commenting out in-progress code.

--- lib/verify_translator.rb
+++ lib/verify_translator.rb
@@ -1,19 +1,19 @@
 require_relative 'block_translator'
 
-class VerifyTranslator < BlockTranslator
-  def initialize(target, target2, block_size)
-
-    # TODO: support dual targets
-
-    @target = target
-    @block_size = block_size
-  end
-
-  def read_block
-    super
-  end
-  
-  def write_block(s)
-    super(s)
-  end
-end
+# class VerifyTranslator < BlockTranslator
+#   def initialize(target, target2, block_size)
+# 
+#     # TODO: support dual targets
+# 
+#     @target = target
+#     @block_size = block_size
+#   end
+# 
+#   def read_block
+#     super
+#   end
+#   
+#   def write_block(s)
+#     super(s)
+#   end
+# end
